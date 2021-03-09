4213bf50e646ff8f4896cbf1d2a7644780632aaa
scm <2318464014@qq.com>
2021-03-09 19:06:50 +0800

    修改了跨域中间件

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // 设置跨域请求头
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization,Origin,Accept,X-Requested-With');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('X-Powered-By', ' 3.2.1');
-    // res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header('Content-Type', 'application/json;charset=utf-8');
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
     } else {
